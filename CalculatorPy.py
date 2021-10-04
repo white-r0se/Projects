60a62ae49aa1e4ad801a45c8bca257f52c64328d
white-r0se <whiteroseraf@gmail.com>
2021-10-04 14:39:07 +0300

    2nd problem

--- CalculatorPy.py
+++ CalculatorPy.py
@@ -104,9 +104,13 @@ lenfrac = {
 }
 
 def ToNum(numst):
+    print(numst)
     if ' и ' in numst:
         wholep, fractp = numst.split(' и ')
         return ToNum(wholep) + Fraction(fractp)
+    elif lenfrac[1] in numst or lenfrac[2] in numst or lenfrac[3] in numst or lenfrac[4] in numst or lenfrac[5] in numst or lenfrac[6] in numst:
+        fractp = numst
+        return Fraction(fractp)
     elif ' ' in numst:
         numst = numst.split()
         return 10 * a2[numst[0]] + a1[numst[1]]
@@ -120,18 +124,49 @@ def ToNum(numst):
         elif numst in a3:
             return(a3[numst])
 
+def CheckFracPeriod(numfr):
+    # numfr = f'{numfr:.11f}'
+    print(numfr)
+    for i in range(4, len(numfr)):
+        if numfr[i] == numfr[i-1] == numfr[i-2] == numfr[i-3] == numfr[i-4] and numfr[i] != '0':
+            return [numfr[i], i-4]
+        elif [numfr[i], numfr[i-1]] == [numfr[i-2], numfr[i-3]] == [numfr[i-4], numfr[i-5]] != ['0', '0'] and i >= 5:
+            return [f'{numfr[i-1]}{numfr[i]}', i-5]
+        elif [numfr[i], numfr[i-1], numfr[i-2]] == [numfr[i-3], numfr[i-4], numfr[i-5]] == [numfr[i-6], numfr[i-7], numfr[i-8]] != ['0', '0', '0'] and i >= 8:
+            return [f'{numfr[i-2]}{numfr[i-1]}{numfr[i]}', i-8]
+        elif [numfr[i], numfr[i-1], numfr[i-2], numfr[i-3]] == [numfr[i-4], numfr[i-5], numfr[i-6], numfr[i-7]] == [numfr[i-8], numfr[i-9], numfr[i-10], numfr[i-11]] != ['0', '0', '0', '0'] and i>=11:
+            return [f'{numfr[i]-3}{numfr[i-2]}{numfr[i-1]}{numfr[i]}', i-11]
+
 def PrintNumb(s):
     if s == '':
         return ''
-    s = round(float(s), 3)
+    print(s)
+    if s == int(s):
+        s = int(s)
     s = str(s)
     p = []
     fr = ''
+    frperiod = False
     lenfr = 0
     if '.' in s:
         s, fr = s.split('.')
         lenfr = len(fr)
-        fr = fr.replace('0', '')
+        if CheckFracPeriod(fr) != None:
+            frperiod = True
+            per, index = CheckFracPeriod(fr)
+            beforper = fr[:index]
+            lenfr = len(beforper)
+        else:
+            s = float(f'{s}.{fr}')
+            s = round(float(s), 6)
+            s = str(s)
+            s, fr = s.split('.')
+            print('New f:', fr)
+            lenfr = len(fr)
+            fr = list(fr)
+            while fr[len(fr)-1] == 0:
+                del fr[len(fr-1)]
+            fr = ''.join(fr)
     if len(s) >= 1:  
         if 9 < int(s[-2:]) < 20:
             p.append(tsatp[int(s[-2:])])
@@ -143,12 +178,38 @@ def PrintNumb(s):
                 p.append(ap1[int(s[-1])])
             else:
                 p.append('ноль')
-        if len(s) == 3:
+        if len(s) >= 3 and int(s[-3]) != 0:
             p.append(ap3[int(s[-3])])
-        if len(s) == 4:
+        if len(s) >= 4 and int(s[-4]) != 0:
             p.append(ap4[int(s[-4])])
+        if len(s) == 5 or len(s) == 6:
+            if int(s[-4]) == 1 and int(s[-5]) != 1:
+                p.append('тысяча')
+            elif int(s[-4]) == 2 and int(s[-5]) != 1:
+                p.append('тысячи')
+            elif int(s[-4]) == 3 and int(s[-5]) != 1:
+                p.append('тысячи')
+            elif int(s[-4]) == 4 and int(s[-5]) != 1:
+                p.append('тысячти')
+            else:
+                p.append('тысяч')
+        
+            num5 = int(s[-5:-3])
+            print('num5', num5)
+            if 9 < num5 < 20:
+                p.append(tsatp[num5])
+            elif num5 > 19:
+                p.append(ap1[int(s[-4])])
+                p.append(ap2[int(s[-5])])
+            if len(s) == 6 and s[-6] != '0':
+                p.append(ap3[int(s[-6])])
     if fr == '':
         return(' '.join(p[::-1]))
+    elif frperiod == True:
+        if lenfr != 0:
+            return(' '.join(p[::-1]) + ' и ' + PrintNumb(beforper) + lenfrac[lenfr] + ' и ' + PrintNumb(per) + ' в периоде')
+        else:
+            return(' '.join(p[::-1]) + ' и ' + PrintNumb(per) + ' в периоде')
     else:
         return(' '.join(p[::-1]) + ' и ' + PrintNumb(fr) + ' ' + lenfrac[lenfr])
 
@@ -192,17 +253,27 @@ def WordCalculator(line):
         return(PrintNumb(a % b))
 
 def Fraction(line):
-    if ' тысячных' in line:
+    if ' миллионных' in line:
+        newline = line.replace(' миллионных', '')
+        return round(0.000001 * ToNum(newline), 6)
+    elif ' стотысячных' in line:
+        newline = line.replace(' стотысячных', '')
+        return round(0.00001 * ToNum(newline), 6)
+    elif ' десятитысячных' in line:
+        newline = line.replace(' десятитысячных', '')
+        return round(0.0001 * ToNum(newline), 6)
+    elif ' тысячных' in line:
         newline = line.replace(' тысячных', '')
-        return round(0.001 * ToNum(newline), 3)
+        return round(0.001 * ToNum(newline), 6)
     elif ' тысячная' in line:
         newline = line.replace(' тысячная', '')
-        return round(0.001 * ToNum(newline), 3)
+        return round(0.001 * ToNum(newline), 6)
     elif ' сотых' in line:
         newline = line.replace(' сотых', '')
-        return round(0.01 * ToNum(newline), 3)
+        return round(0.01 * ToNum(newline), 6)
     elif ' десятых' in line:
         newline = line.replace(' десятых', '')
-        return round(0.1 * ToNum(newline), 3)
+        return round(0.1 * ToNum(newline), 6)
 
-print(WordCalculator('одна шестая умножить на две третьих'))
+print(WordCalculator('девятнадцать и восемьдесят две сотых разделить на девяносто девять'))
+# print(PrintNumb())
