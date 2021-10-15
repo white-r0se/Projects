3a638cad91e969d100ec3bf105dc04a5c261da30
white-r0se <whiteroseraf@gmail.com>
2021-10-15 13:47:15 +0300

    CramerRule

---
+++ CramerRule.py
@@ -0,0 +1,26 @@
+import numpy as np
+from sympy import *
+
+def CramerRule(A, b):
+    n = len(A)
+    det = np.linalg.det(A)
+    x = []
+    for i in range(n):
+        Anew = A.copy()
+        for j in range(n):
+            Anew[j][i] = b[j][0]
+        print(Anew)
+        deti = np.linalg.det(Anew)
+        x.append(round(deti/det, 3))
+    return x
+
+
+A = np.array([
+    [1, 2, 3],
+    [1, 4, 9],
+    [1, 8, 27]
+])
+b = np.array([
+    [4, 16, 46]
+]).reshape((3, 1))
+print(CramerRule(A, b))
